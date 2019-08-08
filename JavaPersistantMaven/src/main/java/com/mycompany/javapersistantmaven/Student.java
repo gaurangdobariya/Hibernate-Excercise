542d0af3c8f4bfc02fbf6c19bca405ef85e759b9
Gaurang <dobariyagaurang@gmail.com>
2019-08-08 16:20:28 +0530

    Resolved Discussion

--- JavaPersistantMaven/src/main/java/com/mycompany/javapersistantmaven/Student.java
+++ JavaPersistantMaven/src/main/java/com/mycompany/javapersistantmaven/Student.java
@@ -1,6 +1,5 @@
 package com.mycompany.javapersistantmaven;
 
-import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,7 +13,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name= "student")
-public class Student implements Serializable{
+public class Student{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
